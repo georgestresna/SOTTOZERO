cf736b19470027bc833c6bcb3f537aa66c9b0a77
George Stresna <68332128+georgestresna@users.noreply.github.com>
2022-09-20 18:30:12 +0300

    dsdsd

--- meniu/script.js
+++ meniu/script.js
@@ -57,14 +57,232 @@ const iteme=[
     },
 ]
 
+const iteme_glovo=[
+    //meniuri
+    [{
+        nume:'Meniu Burger Epic Dreams',
+        gramaj:'700',
+        ingrediente:'Chifla, carne vita + porc, somon fume, mix branzeturi, ceapa prajita, rosii cherry, sos cheddar, ardei iute, salata iceberg, sosuri, Cartofi prajiti aromatizati',
+        pret:'45.00',
+        img:'lb8h9oxocmnihkwmswmq',
+        color:'#D73B3B'
+    },
+    {
+        nume:'Meniu Burger General',
+        gramaj:'700',
+        ingrediente:'Chifla, carne vita + porc, mix branzeturi, ou, bacon, rosii uscate, ceapa prajita, salata iceberg, sosuri, Cartofi prajiti aromatizati',
+        pret:'45.00',
+        img:'',
+        color:'#D73B3B'
+    },
+    {
+        nume:'Meniu Burger Colonel',
+        gramaj:'700',
+        ingrediente:'Chiflă ,carne de vită + porc, mix brânzeturi, salată iceberg, sosuri, rosii uscate, ceapă prăjită, bacon, jalapeno, Cartofi prajiti aromatizati',
+        pret:'40.00',
+        img:'',
+        color:'#D73B3B'
+    },
+    {
+        nume:'Meniu Burger Maior',
+        gramaj:'700',
+        ingrediente:'Chiflă ,carne de vită + porc, mix brânzeturi, salată iceberg, sosuri, rosii uscate, ceapă prăjită, bacon, Cartofi prajiti aromatizati',
+        pret:'40.00',
+        img:'',
+        color:'#D73B3B'
+    },
+    {
+        nume:'Meniu Burger Clasic',
+        gramaj:'700',
+        ingrediente:'Chifla, chiftea vita + porc, cheddar, salata iceberg, ceapa rosie, castraveti in otet, maioneza de casa, ketchup, mustar, Cartofi prajiti aromatizati',
+        pret:'40.00',
+        img:'',
+        color:'#D73B3B'
+    },
+    {
+        nume:'Meniu Crispy',
+        gramaj:'330',
+        ingrediente:'Carne de pui, ou, pesmet, cartofi prajiti aromatizati',
+        pret:'25.00',
+        img:'',
+        color:'#D73B3B'
+    },
+    {
+        nume:'Meniu Sandwich Pui/ Porc/ Ceafa Afumata',
+        gramaj:'500',
+        ingrediente:'Chifla, chips mix branzeturi, pulpa de pui grill/ ceafa de porc grill/ ceafa de porc afumata, salata iceberg, ketchup, maioneza de casa, Cartofi prajiti aromatizati',
+        pret:'22.00',
+        img:'',
+        color:'#D73B3B'
+    }],
+    // burgeri
+    [{
+        nume:'Burger Epic Dreams',
+        gramaj:'500',
+        ingrediente:'Chifla, carne vita + porc, somon fume, mix branzeturi, ceapa prajita, rosii cherry, sos cheddar, ardei iute, salata iceberg, sosuri',
+        pret:'40.00',
+        img:'',
+        color:'#364B94'
+    },
+    {
+        nume:'Burger General',
+        gramaj:'500',
+        ingrediente:'Chifla, carne vita + porc, mix branzeturi, ou, bacon, rosii uscate, ceapa prajita, salata iceberg, sosuri',
+        pret:'40.00',
+        img:'',
+        color:'#364B94'
+    },
+    {
+        nume:'Burger Colonel',
+        gramaj:'500',
+        ingrediente:'Chiflă ,carne de vită + porc, mix brânzeturi, salată iceberg, sosuri, rosii uscate, ceapă prăjită, bacon, jalapeno',
+        pret:'35.00',
+        img:'',
+        color:'#364B94'
+    },
+    {
+        nume:'Burger Maior',
+        gramaj:'500',
+        ingrediente:'Chiflă ,carne de vită + porc, mix brânzeturi, salată iceberg, sosuri, rosii uscate, ceapă prăjită, bacon',
+        pret:'35.00',
+        img:'',
+        color:'#364B94'
+    },
+    {
+        nume:'Burger Clasic',
+        gramaj:'500',
+        ingrediente:'Chifla, chiftea vita + porc, cheddar, salata iceberg, ceapa rosie, castraveti in otet, maioneza de casa, ketchup, mustar',
+        pret:'35.00',
+        img:'',
+        color:'#364B94'
+    }],
+    //burgeri mini
+    [{
+        nume:'Mini Epic Dreams',
+        gramaj:'300',
+        ingrediente:'Chifla, carne vita + porc, somon fume, mix branzeturi, ceapa prajita, rosii cherry, sos cheddar, ardei iute, salata iceberg, sosuri',
+        pret:'25.00',
+        img:'',
+        color:'rgb(44, 91, 44)'
+    },
+    {
+        nume:'Mini General',
+        gramaj:'300',
+        ingrediente:'Chifla, carne vita + porc, mix branzeturi, ou, bacon, rosii uscate, ceapa prajita, salata iceberg, sosuri',
+        pret:'23.00',
+        img:'',
+        color:'rgb(44, 91, 44)'
+    },
+    {
+        nume:'Mini Colonel',
+        gramaj:'300',
+        ingrediente:'Chiflă ,carne de vită + porc, mix brânzeturi, salată iceberg, sosuri, rosii uscate, ceapă prăjită, bacon, jalapeno',
+        pret:'20.00',
+        img:'',
+        color:'rgb(44, 91, 44)'
+    },
+    {
+        nume:'Mini Maior',
+        gramaj:'300',
+        ingrediente:'Chiflă ,carne de vită + porc, mix brânzeturi, salată iceberg, sosuri, rosii uscate, ceapă prăjită, bacon',
+        pret:'20.00',
+        img:'',
+        color:'rgb(44, 91, 44)'
+    },
+    {
+        nume:'Mini Clasic',
+        gramaj:'300',
+        ingrediente:'Chifla, chiftea vita + porc, cheddar, salata iceberg, ceapa rosie, castraveti in otet, maioneza de casa, ketchup, mustar',
+        pret:'18.00',
+        img:'',
+        color:'rgb(44, 91, 44)'
+    }],
+    //pizza
+    [{
+        nume:'PIZZA BURGER',
+        gramaj:'800',
+        ingrediente:'blat de cartofi prajiti, sos branzeturi, carne vita+porc, bacon, sosuri, salata, jalapeno',
+        pret:'40.00',
+        img:'pizza-burger.jpg',
+        color:'rgb(227, 147, 0)'
+    },
+    {
+        nume:'PIZZA NACHOS',
+        gramaj:'500',
+        ingrediente:'blat nachos, sos branzeturi, carne de pui, sosuri, jalapeno',
+        pret:'40.00',
+        img:'pizza-nachos.jpg',
+        color:'rgb(227, 147, 0)'
+    },
+    {
+        nume:'PIZZA SOTTOZERO',
+        gramaj:'800',
+        ingrediente:'blat de cartofi prajiti,sos de branzeturi,carne de porc + pui +vita, sosuri, salata, jalapeno',
+        pret:'40.00',
+        img:'pizza_sottozero.jpg',
+        color:'rgb(227, 147, 0)'
+    }],
+    //burito si quesadilla
+    [{
+        nume:'Quesadilla Gran Sabor',
+        gramaj:'350',
+        ingrediente:'lipie, carne de pui+porc+vita, branzeturi, salate, sosuri, ceapa aromatizata, jalapeno',
+        pret:'32.00',
+        img:'quesadilla.jpg',
+        color:'#928940'
+    },
+    {
+        nume:'Quesadilla de Pui/Porc',
+        gramaj:'350',
+        ingrediente:'lipie, carne de pui/porc, branzeturi, salate, sosuri, ceapa aromatizata, jalapeno',
+        pret:'28.00',
+        img:'quesadilla.jpg',
+        color:'#928940'
+    },
+    {
+        nume:'BURRITO PUI/PORC',
+        gramaj:'450',
+        ingrediente:'lipie, carne de pui/porc, branzeturi, mix Mexican, salate, sosuri, jalapeno',
+        pret:'30.00',
+        img:'burrito.jpg',
+        color:'#928940'
+    },
+    {
+        nume:'BURRITO Gran Sabor',
+        gramaj:'450',
+        ingrediente:'Lipie, carne de pui + porc + vita, bacon, mix branzeturi, mix mexican, jalapeno',
+        pret:'35.00',
+        img:'burrito.jpg',
+        color:'#928940'
+    }],
+    //sandwich
+    [{
+        nume:'Sandwich cu Pui/ Porc/ Ceafa Afumata',
+        gramaj:'300',
+        ingrediente:'Chifla, pulpa de pui grill/ ceafa de porc la grill/ ceafa afumata, chips branzeturi, salata iceberg, ketchup, maioneza de casa',
+        pret:'17.00',
+        img:'',
+        color:'rgb(59, 146, 114)'
+    },
+    {
+        nume:'Toast Sottozero',
+        gramaj:'200',
+        ingrediente:'Paine toast, ceafa afumata, cascaval, sosul casei',
+        pret:'10.00',
+        img:'',
+        color:'rgb(59, 146, 114)'
+    }],
+]
 
-const myContent = document.querySelector(".options");
+function renderJS(path,order){
+
+const myContent1 = document.querySelector(path);
 
 window.addEventListener("DOMContentLoaded", function () {
-    const showInHtml = iteme.map((project) => {
+    const showInHtml = iteme_glovo[order].map((project) => {
         return `
-        <div class="meal">
-        <img src="meniu_prod/${project.img}" alt="">
+        <div class="meal" style="background-color:${project.color};">
+        <img src="meniu_g/${project.img}" alt="">
         <div class="content">
             <div class="text">
                 <h3>${project.nume}</h3>
@@ -75,5 +293,13 @@ window.addEventListener("DOMContentLoaded", function () {
         </div>
         </div>`;
     })
-myContent.innerHTML = showInHtml;
+myContent1.innerHTML = showInHtml;
 });
+};
+
+renderJS('.options .meniu_meniu',0);
+renderJS('.options .meniu_burgeri',1);
+renderJS('.options .meniu_miniburgeri',2);
+renderJS('.options .meniu_pizza',3);
+renderJS('.options .meniu_buritoquesadilla',4);
+renderJS('.options .meniu_sandwich',5);
